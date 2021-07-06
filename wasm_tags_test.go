dfe6ac204d6120da70a1e16bdea305621da2042d
John Starich <johnstarich@johnstarich.com>
2021-07-05 19:04:43 -0500

    Add tests for Go build tag setup and import detection

---
+++ wasm_tags_test.go
@@ -0,0 +1,65 @@
+// +build !wasm
+
+package hackpadfs
+
+import (
+	"bufio"
+	"go/build/constraint"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestWasmTags(t *testing.T) {
+	walkErr := filepath.Walk(".", func(path string, info fs.FileInfo, err error) error {
+		switch {
+		case err != nil:
+			return err
+		case !(strings.HasPrefix(filepath.FromSlash(path), "os/") && strings.HasSuffix(path, "_test.go")):
+			// For now, only scan os/*_test.go files. May expand later to other packages and tests that call unimplemented JS functions.
+			return nil
+		case info.IsDir():
+			return nil
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			line := strings.TrimSpace(scanner.Text())
+			if line == "" {
+				continue
+			}
+			if !strings.HasPrefix(line, "//") {
+				// hit non-comment line, so no build tags exist (see https://golang.org/cmd/go/#hdr-Build_constraints)
+				t.Errorf("File %q does not contain a !wasm build tag", path)
+				break
+			}
+
+			expr, err := constraint.Parse(line)
+			if err != nil {
+				t.Logf("Build constraint failed to parse line in file %q: %q; %v", path, line, err)
+				continue
+			}
+			if isExcludeWasm(expr) {
+				break
+			}
+		}
+		return scanner.Err()
+	})
+	if walkErr != nil {
+		t.Error("Walk failed:", walkErr)
+	}
+}
+
+func isExcludeWasm(expr constraint.Expr) bool {
+	if expr, ok := expr.(*constraint.NotExpr); ok {
+		return expr.X.String() == "wasm"
+	}
+	return false
+}
