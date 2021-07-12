a50944bdb953cd9363fea9bf81760a7d04551052
John Starich <johnstarich@johnstarich.com>
2021-07-12 01:17:11 -0500

    Add mem.FS. Moved from keyvalue tests, exported, and fewer locks

--- mem/fs_test.go
+++ mem/fs_test.go
@@ -1,20 +1,20 @@
-package keyvalue_test
+package mem
 
 import (
 	"testing"
 
 	"github.com/hack-pad/hackpadfs/fstest"
-	"github.com/hack-pad/hackpadfs/mem"
+	"github.com/hack-pad/hackpadfs/internal/assert"
 )
 
 func TestFS(t *testing.T) {
 	t.Parallel()
 	options := fstest.FSOptions{
-		Name: "keyvalue",
+		Name: "mem",
 		TestFS: func(tb testing.TB) fstest.SetupFS {
-			fs, err := mem.NewFS() // mem.FS uses keyvalue.Store under the hood. Run the tests again here for keyvalue package coverage
-			if err != nil {
-				tb.Fatal(err)
+			fs, err := NewFS()
+			if !assert.NoError(tb, err) {
+				tb.FailNow()
 			}
 			return fs
 		},
---
+++ mem/store.go
@@ -0,0 +1,195 @@
+package mem
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/hack-pad/hackpadfs"
+	"github.com/hack-pad/hackpadfs/keyvalue"
+	"github.com/hack-pad/hackpadfs/keyvalue/blob"
+)
+
+type store struct {
+	mu      sync.Mutex
+	records sync.Map
+}
+
+func newStore() *store {
+	return &store{}
+}
+
+type fileRecord struct {
+	store   *store
+	path    string
+	data    blob.Blob
+	mode    hackpadfs.FileMode
+	modTime time.Time
+}
+
+func (f fileRecord) Data() (blob.Blob, error) {
+	return blob.NewBytes(f.data.Bytes()).Slice(0, int64(f.data.Len()))
+}
+
+func (f fileRecord) Size() int64              { return int64(f.data.Len()) }
+func (f fileRecord) Mode() hackpadfs.FileMode { return f.mode }
+func (f fileRecord) ModTime() time.Time       { return f.modTime }
+func (f fileRecord) Sys() interface{}         { return nil }
+
+func (f fileRecord) ReadDirNames() ([]string, error) {
+	var names []string
+	prefix := f.path + "/"
+	if f.path == "." {
+		prefix = ""
+	}
+
+	f.store.records.Range(func(key, _ interface{}) bool {
+		p := key.(string)
+		if strings.HasPrefix(p, prefix) {
+			p = strings.TrimPrefix(p, prefix)
+			if !strings.ContainsRune(p, '/') {
+				names = append(names, p)
+			}
+		}
+		return true
+	})
+	return names, nil
+}
+
+func (f fileRecord) String() string {
+	dataStr := "dir"
+	if !f.mode.IsDir() {
+		dataStr = fmt.Sprintf("%dB", f.data.Len())
+	}
+	return fmt.Sprintf("%q: %s %s %s", f.path, f.mode, dataStr, f.modTime.Format(time.RFC3339))
+}
+
+func (s *store) Get(ctx context.Context, path string) (keyvalue.FileRecord, error) {
+	value, ok := s.records.Load(path)
+	if !ok {
+		return nil, hackpadfs.ErrNotExist
+	}
+	record := value.(keyvalue.FileRecord)
+	return record, nil
+}
+
+func (s *store) Set(ctx context.Context, path string, src keyvalue.FileRecord) error {
+	var contents blob.Blob
+	if src != nil {
+		var err error
+		contents, err = src.Data()
+		if err != nil {
+			return err
+		}
+	}
+	return s.set(path, src, contents)
+}
+
+func (s *store) set(path string, src keyvalue.FileRecord, contents blob.Blob) error {
+	if src == nil {
+		s.records.Delete(path)
+	} else {
+		data, err := src.Data()
+		if err != nil {
+			return err
+		}
+		record := fileRecord{
+			store:   s,
+			path:    path,
+			data:    data,
+			mode:    src.Mode(),
+			modTime: src.ModTime(),
+		}
+		s.records.Store(path, record)
+	}
+	return nil
+}
+
+type transaction struct {
+	ctx     context.Context
+	abort   context.CancelFunc
+	store   *store
+	op      keyvalue.OpID
+	results []keyvalue.OpResult
+}
+
+func (s *store) Transaction() (keyvalue.Transaction, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	txn := &transaction{
+		ctx:   ctx,
+		abort: cancel,
+		store: s,
+	}
+	s.mu.Lock()
+	return txn, nil
+}
+
+func (t *transaction) prepOp() (keyvalue.OpID, error) {
+	select {
+	case <-t.ctx.Done():
+		return 0, t.ctx.Err()
+	default:
+	}
+
+	op := t.op
+	t.op++
+	return op, nil
+}
+
+func (t *transaction) Get(path string) keyvalue.OpID {
+	return t.GetHandler(path, keyvalue.OpHandlerFunc(func(txn keyvalue.Transaction, result keyvalue.OpResult) error {
+		return nil
+	}))
+}
+
+func (t *transaction) GetHandler(path string, handler keyvalue.OpHandler) keyvalue.OpID {
+	op, err := t.prepOp()
+	if err != nil {
+		t.results = append(t.results, keyvalue.OpResult{Op: op, Err: err})
+		return op
+	}
+	record, err := t.store.Get(t.ctx, path)
+	result := keyvalue.OpResult{Op: op, Record: record, Err: err}
+	err = handler.Handle(t, result)
+	if result.Err == nil && err != nil {
+		result.Err = err
+	}
+	t.results = append(t.results, result)
+	return op
+}
+
+func (t *transaction) Set(path string, src keyvalue.FileRecord, contents blob.Blob) keyvalue.OpID {
+	return t.SetHandler(path, src, contents, keyvalue.OpHandlerFunc(func(txn keyvalue.Transaction, result keyvalue.OpResult) error {
+		return nil
+	}))
+}
+
+func (t *transaction) SetHandler(path string, src keyvalue.FileRecord, contents blob.Blob, handler keyvalue.OpHandler) keyvalue.OpID {
+	op, err := t.prepOp()
+	if err != nil {
+		t.results = append(t.results, keyvalue.OpResult{Op: op, Err: err})
+		return op
+	}
+	err = t.store.set(path, src, contents)
+	result := keyvalue.OpResult{Op: op, Err: err}
+	err = handler.Handle(t, result)
+	if result.Err == nil && err != nil {
+		result.Err = err
+	}
+	t.results = append(t.results, result)
+	return op
+}
+
+func (t *transaction) Commit(ctx context.Context) ([]keyvalue.OpResult, error) {
+	t.abort()
+	t.store.mu.Unlock()
+	return t.results, nil
+}
+
+func (t *transaction) Abort() error {
+	t.abort()
+	t.store.mu.Unlock()
+	return nil
+}
