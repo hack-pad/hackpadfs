fdd02cd593ccde9125adedbdb3ebb54cd0dd5cf6
John Starich <johnstarich@johnstarich.com>
2021-07-14 00:45:21 -0500

    mount: Fix data race where bad mount exists while processing, add tests for AddMount

--- mount/fs_test.go
+++ mount/fs_test.go
@@ -1,6 +1,8 @@
 package mount_test
 
 import (
+	"errors"
+	"sync"
 	"testing"
 	"time"
 
@@ -13,22 +15,131 @@ import (
 
 func TestFS(t *testing.T) {
 	t.Parallel()
+
+	requireNoError := func(tb testing.TB, err error) {
+		if !assert.NoError(tb, err) {
+			tb.FailNow()
+		}
+	}
+
 	options := fstest.FSOptions{
 		Name: "mount",
 		TestFS: func(tb testing.TB) fstest.SetupFS {
 			mem, err := mem.NewFS()
-			if !assert.NoError(tb, err) {
-				tb.FailNow()
-			}
+			requireNoError(tb, err)
 			fs, err := mount.NewFS(mem)
-			if !assert.NoError(tb, err) {
-				tb.FailNow()
-			}
+			requireNoError(tb, err)
 			return &allMountFS{fs}
 		},
 	}
 	fstest.FS(t, options)
 	fstest.File(t, options)
+
+	options = fstest.FSOptions{
+		Name: "mount unused",
+		TestFS: func(tb testing.TB) fstest.SetupFS {
+			memRoot, err := mem.NewFS()
+			requireNoError(tb, err)
+			requireNoError(tb, memRoot.Mkdir("foo", 0666))
+			memUnused, err := mem.NewFS()
+			requireNoError(tb, err)
+			fs, err := mount.NewFS(memRoot)
+			requireNoError(tb, fs.AddMount("unused", memUnused))
+			return &allMountFS{fs}
+		},
+	}
+	fstest.FS(t, options)
+	fstest.File(t, options)
+}
+
+func TestAddMount(t *testing.T) {
+	newFS := func(t *testing.T) *mount.FS {
+		memRoot, err := mem.NewFS()
+		assert.NoError(t, err)
+		fs, err := mount.NewFS(memRoot)
+		assert.NoError(t, err)
+		return fs
+	}
+
+	t.Run("invalid path", func(t *testing.T) {
+		fs := newFS(t)
+		memFoo, err := mem.NewFS()
+		assert.NoError(t, err)
+		err = fs.AddMount("foo/../foo", memFoo)
+		assert.Error(t, err)
+		assert.Equal(t, true, errors.Is(err, hackpadfs.ErrInvalid))
+		assert.Equal(t, 0, len(fs.MountPoints()))
+	})
+
+	t.Run("no file at mount point", func(t *testing.T) {
+		fs := newFS(t)
+		memFoo, err := mem.NewFS()
+		assert.NoError(t, err)
+		err = fs.AddMount("foo", memFoo)
+		assert.Error(t, err)
+		assert.Equal(t, true, errors.Is(err, hackpadfs.ErrNotExist))
+		assert.Equal(t, 0, len(fs.MountPoints()))
+	})
+
+	t.Run("mount point not a directory", func(t *testing.T) {
+		fs := newFS(t)
+		memFoo, err := mem.NewFS()
+		assert.NoError(t, err)
+		f, err := hackpadfs.Create(fs, "foo")
+		assert.NoError(t, err)
+		assert.NoError(t, f.Close())
+
+		err = fs.AddMount("foo", memFoo)
+		assert.Error(t, err)
+		assert.Equal(t, true, errors.Is(err, hackpadfs.ErrNotDir))
+		assert.Equal(t, 0, len(fs.MountPoints()))
+	})
+
+	t.Run("mount point already exists", func(t *testing.T) {
+		fs := newFS(t)
+		memFoo, err := mem.NewFS()
+		assert.NoError(t, err)
+		assert.NoError(t, hackpadfs.Mkdir(fs, "foo", 0700))
+		err = fs.AddMount("foo", memFoo)
+		assert.NoError(t, err)
+
+		err = fs.AddMount("foo", memFoo)
+		assert.Error(t, err)
+		assert.Equal(t, true, errors.Is(err, hackpadfs.ErrExist))
+		assert.Equal(t, []mount.Point{
+			{Path: "foo"},
+		}, fs.MountPoints())
+	})
+
+	t.Run("concurrent conflicting mounts only succeed once", func(t *testing.T) {
+		fs := newFS(t)
+		memFoo, err := mem.NewFS()
+		assert.NoError(t, err)
+		assert.NoError(t, hackpadfs.Mkdir(fs, "foo", 0700))
+
+		var wg sync.WaitGroup
+		const maxAttempts = 3
+		wg.Add(maxAttempts)
+		errs := make([]error, maxAttempts)
+		for i := range errs {
+			go func(i int) {
+				defer wg.Done()
+				errs[i] = fs.AddMount("foo", memFoo)
+			}(i)
+		}
+		wg.Wait()
+
+		isNil := 0
+		for i := range errs {
+			if errs[i] == nil {
+				isNil++
+			}
+		}
+		assert.Equal(t, 1, isNil)
+		assert.Equal(t, []mount.Point{
+			{Path: "foo"},
+		}, fs.MountPoints())
+	})
 }
 
 // allMountFS wraps a mount.FS with the usual functions implemented by a mounted file system, so fstest won't skip the capability-based tests
