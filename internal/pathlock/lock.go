c0d176de33ad3196f05dc93d66123c8d625eba00
John Starich <johnstarich@johnstarich.com>
2021-07-27 19:26:05 -0500

    cache: Fix lint

--- internal/pathlock/lock.go
+++ internal/pathlock/lock.go
@@ -2,22 +2,26 @@ package pathlock
 
 import "sync"
 
-type Lock struct {
+// Mutex is a path-based locker. Lock a given path for exclusive access to that path.
+type Mutex struct {
 	pathLocks sync.Map
 }
 
-func New() *Lock {
-	return &Lock{}
+// New returns a new Mutex
+func New() *Mutex {
+	return &Mutex{}
 }
 
-func (l *Lock) Lock(path string) {
+// Lock blocks access to 'path' until Unlock is called
+func (l *Mutex) Lock(path string) {
 	var newMu sync.Mutex
 	muInterface, _ := l.pathLocks.LoadOrStore(path, &newMu)
 	mu := muInterface.(*sync.Mutex)
 	mu.Lock()
 }
 
-func (l *Lock) Unlock(path string) {
+// Unlock unblocks access to 'path'
+func (l *Mutex) Unlock(path string) {
 	muInterface, _ := l.pathLocks.Load(path)
 	mu := muInterface.(*sync.Mutex)
 	mu.Unlock()
